e4beddc41bc8fae30c872d2585ae56ac7ef59fe9
joshpueyo12-netizen <joshpueyo12@gmail.com>
2026-06-10 21:40:45 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,7 @@
-import { defineConfig } from "@lovable.dev/vite-tanstack-config";
+tsimport { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
   tanstackStart: {
     server: { entry: "server" },
   },
-  vite: {
-    build: {
-      ssr: false,
-    }
-  }
 });
