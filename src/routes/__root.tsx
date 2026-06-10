10e9bb616d88c3d42f3187a163f68692acaed1da
joshpueyo12-netizen <joshpueyo12@gmail.com>
2026-06-10 19:11:40 +0800

    Update __root.tsx

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -122,12 +122,14 @@ function RootShell({ children }: { children: ReactNode }) {
     </html>
   );
 }
-export function RootComponent() {
+
+function RootComponent() {
+  const { queryClient } = Route.useRouteContext();
+
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
+      {/* Required: nested routes render here. Removing <Outlet /> breaks all child routes. */}
       <Outlet />
-    </>
+    </QueryClientProvider>
   );
 }
-e
-
