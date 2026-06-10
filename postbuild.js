6e071608892af83d0a324e71f4af2d6b0ae77dac
joshpueyo12-netizen <joshpueyo12@gmail.com>
2026-06-10 22:18:43 +0800

    Update postbuild.js

--- postbuild.js
+++ postbuild.js
@@ -1,4 +1,8 @@
 const fs = require('fs');
+const path = require('path');
 try {
-  fs.copyFileSync('_redirects', 'dist/client/_redirects');
+  fs.copyFileSync(
+    path.join(__dirname, '_redirects'),
+    path.join(__dirname, 'dist/client/_redirects')
+  );
 } catch(e) {}
